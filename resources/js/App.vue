387928f8b2ad2bc35a8d5050be7e8b49903ad2bb
THEC0Der7 <aa4223@live.mdx.ac.uk>
2021-12-14 00:28:21 +0500

    vuetify added

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app app>
+    <v-container>
+      <v-btn color="primary"> Primary </v-btn>
+      <v-btn color="secondary"> Secondary </v-btn>
+      <v-btn color="error"> Error </v-btn>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
